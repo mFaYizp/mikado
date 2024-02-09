af42d86ef49391b77811fc2fae8c7e35597ae5d0
mFaYizp <fayizfaz78@gmail.com>
2024-02-09 10:21:54 +0530

    accordion

--- client/src/components/careers/Career.jsx
+++ client/src/components/careers/Career.jsx
@@ -4,19 +4,19 @@ import AddIcon from "@mui/icons-material/Add";
 import RemoveIcon from "@mui/icons-material/Remove";
 
 const Career = () => {
-  const [show, setShow] = useState(true);
+  const [show, setShow] = useState(false);
   return (
     <section className="career-section">
       <div className="border" />
       <div className="drop">
         {show ? (
-          <AddIcon
+          <RemoveIcon
             fontSize="large"
             onClick={() => setShow(false)}
             className="drop-icon"
           />
         ) : (
-          <RemoveIcon
+          <AddIcon
             fontSize="large"
             onClick={() => setShow(true)}
             className="drop-icon"
@@ -36,33 +36,50 @@ const Career = () => {
           <h2 className="job-requirement">
             5+ Years of Product Marketing Experience
           </h2>
-          <p className="job-skills">
+          <div className="job-skills">
             <span className="job-skills-header"> Skills -</span>
-            <ul>
+            <ul className={show ? "job-skills-hide" : "job-skills-single-show"}>
               <li>
                 Experience managing and measuring marketing performance in
                 various channels{" "}
               </li>
-              <li>
-                Product marketing experience and familiarity with generative AI
-                or developer audiences{" "}
-              </li>
-              <li> Strong Communication and Presentation skills.</li>
-              <li> Ability to meet tight deadlines with high quality work. </li>
-              <li> Strong knowledge of AI & Saas Tools.</li>
             </ul>
-          </p>
-          <h2 className="job-requirement"> Job Description:</h2>
-          <p className="job-skills">
-            As a Product Marketing Manager, you will engage our customers and
-            prospects to help them understand how our next generation AI tools
-            and services that helps them be more productive, while also
-            entertaining audiences building products with global intellectual
-            properties.
-          </p>
-          <div className="button-container">
-            <button className="apply-button">Apply Now</button>
           </div>
+          {show && (
+            <div
+              className={show ? "job-skills-single-show" : "job-skills-hide"}
+            >
+              <div>
+                <ul>
+                  <li>
+                    Experience managing and measuring marketing performance in
+                    various channels{" "}
+                  </li>
+                  <li>
+                    Product marketing experience and familiarity with generative
+                    AI or developer audiences{" "}
+                  </li>
+                  <li> Strong Communication and Presentation skills.</li>
+                  <li>
+                    {" "}
+                    Ability to meet tight deadlines with high quality work.{" "}
+                  </li>
+                  <li> Strong knowledge of AI & Saas Tools.</li>
+                </ul>
+              </div>
+              <h2 className="job-requirement"> Job Description:</h2>
+              <p className="job-skills">
+                As a Product Marketing Manager, you will engage our customers
+                and prospects to help them understand how our next generation AI
+                tools and services that helps them be more productive, while
+                also entertaining audiences building products with global
+                intellectual properties.
+              </p>
+              <div className="button-container">
+                <button className="apply-button">Apply Now</button>
+              </div>
+            </div>
+          )}
         </div>
       </div>
       <div className="border" style={{ marginTop: "30px" }} />
